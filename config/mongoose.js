bcc0ac754ee004bed47437286de19515cf48c582
Sri Hari Jagan <100404902+SriHariJagan@users.noreply.github.com>
2024-03-25 14:54:07 +0530

    mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1/HabitTracker");
+mongoose.connect("mongodb://0.0.0.0:27017/HabitTracker");
 
 const db = mongoose.connection;
 
